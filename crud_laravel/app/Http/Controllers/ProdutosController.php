b7f9589f6fd9f9e542d9d8c574f46cb8812360dc
I.A.Gzyk <ivangzyk@hotmail.com>
2021-01-07 22:53:49 -0300

    crud_1.5

--- crud_laravel/app/Http/Controllers/ProdutosController.php
+++ crud_laravel/app/Http/Controllers/ProdutosController.php
@@ -25,4 +25,32 @@ class ProdutosController extends Controller
         $produto = Produto::findOrFail($id);
         return view('produtos.show', ['produto' => $produto]);
     }
+    
+    public function edit($id) {
+        $produto = Produto::findOrFail($id);
+        return view('produto.edit', ['produto' => $produto]);
+    }
+    
+    public function update(Request $request, $id){
+        $produto = Produto::findOrFail($id);
+        
+        $produto->update([
+            'nome' => $request->nome,
+            'custo' => $request->custo,
+            'preco' => $request->preco,
+            'quantidade' => $request->quantidade,
+        ]);
+        return "Produto atualizado com sucesso!";
+    }
+    
+    public function delete($id) {
+        $produto = Produto::findOrFail($id);
+        return view('produtos.delete', ['produto' => $produto]);
+    }
+    
+    public function destroy($id) {
+        $produto = Produto::findOrFail($id);
+        $produto->delete();
+        return "Produto excluido com sucesso!";
+    }
 }
